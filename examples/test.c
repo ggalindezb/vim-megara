729bd9b1938b66306cd2f691ba8f4d95782f1ff1
Gerardo Galindez <gerardo.galindez@gmail.com>
2012-12-01 01:09:50 -0600

    Readded dev files

---
+++ examples/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define MACRO = 1
+
+int globalVar = 5;
+
+int main(void)
+{
+  int x = 1;
+  return 0;
+}
---
+++ examples/test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class test {
+
+  public static final int WHATEVER = 1;
+  private int x;
+  private double y;
+
+  public static void main(String[] args) {
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    System.out.println(list);
+  }
+}
